1e823d6f789550a19fa57595f63a72958e57158e
Garik <44021019+Garo1995@users.noreply.github.com>
2025-04-10 18:49:39 +0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+
+
+let globalSwiper = new Swiper(".slideLab-slider", {
+    direction: "vertical",
+    slidesPerView: 1,
+    spaceBetween: 1,
+    mousewheel: true,
+    // speed: 2000,
+
+});
+
+
